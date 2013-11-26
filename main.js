d01b06a1c7b3aac99753b6611c80187d4ae1103c
dristic <dristic101@gmail.com>
2013-11-26 15:59:47 -0800

    Add basic ticker.

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 // BitCoin stream consumer
 // mtgox channels: https://mtgox.com/api/2/stream/list_public?pretty
+document.addEventListener('DOMContentLoaded', function () {
+  var pubnub = PUBNUB.init({
+    subscribe_key: 'sub-c-50d56e1e-2fd9-11e3-a041-02ee2ddab7fe'
+  });
 
-var pubnub = PUBNUB.init({
-  subscribe_key: 'sub-c-50d56e1e-2fd9-11e3-a041-02ee2ddab7fe'
-});
+  var priceEl = document.querySelector('#price');
 
-pubnub.subscribe({
-  channel: 'd5f06780-30a8-4a48-a2f8-7ed181b4a13f',
-  callback: function (message) {
-    console.log("BitCoin Message: ", message);
-  }
+  pubnub.subscribe({
+    channel: 'd5f06780-30a8-4a48-a2f8-7ed181b4a13f',
+    callback: function (message) {
+      console.log("BitCoin Message: ", message);
+
+      if (message.ticker) {
+        priceEl.innerHTML = message.ticker.avg.display;
+      }
+    }
+  });
+
+  setTimeout(function () {
+    pubnub.unsubscribe({
+      channel: 'd5f06780-30a8-4a48-a2f8-7ed181b4a13f'
+    });
+  }, 3000);
 });
 
