fd2ca504233321495fbc296e0e7b46a65c7f5e5d
dristic <dristic101@gmail.com>
2013-11-26 17:58:24 -0800

    Add d3 bar chart.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // BitCoin stream consumer
 // mtgox channels: https://mtgox.com/api/2/stream/list_public?pretty
 document.addEventListener('DOMContentLoaded', function () {
+  var data = [];
+
   var pubnub = PUBNUB.init({
     subscribe_key: 'sub-c-50d56e1e-2fd9-11e3-a041-02ee2ddab7fe'
   });
@@ -14,14 +16,33 @@ document.addEventListener('DOMContentLoaded', function () {
 
       if (message.ticker) {
         priceEl.innerHTML = message.ticker.avg.display;
+
+        var value = parseFloat(message.ticker.avg.value);
+        data.push(value);
+
+        d3.select('.chart')
+          .selectAll('div')
+            .data(data)
+          .enter().append("div")
+            .transition()
+            .style("width", width)
+            .text(text);
       }
     }
   });
 
-  setTimeout(function () {
-    pubnub.unsubscribe({
-      channel: 'd5f06780-30a8-4a48-a2f8-7ed181b4a13f'
-    });
-  }, 3000);
+  function width(d) {
+    return d / 10 + "px";
+  };
+  function text(d) {
+    return d;
+  };
+
+  d3.select('.chart')
+    .selectAll('div')
+      .data(data)
+    .enter().append("div")
+      .style("width", width)
+      .text(text);
 });
 
